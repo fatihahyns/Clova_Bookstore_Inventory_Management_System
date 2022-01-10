8e8fb79585c88c35b353b80883b9fa58c64d91b8
Fatihah Yunus <nurfatihahyunus7@gmail.com>
2022-01-10 20:03:43 +0800

    commit customers class (getter and setter)

--- src/main/java/com/example/clova_bookstore_inventory_management_system/customers.java
+++ src/main/java/com/example/clova_bookstore_inventory_management_system/customers.java
@@ -1,4 +1,40 @@
 package com.example.clova_bookstore_inventory_management_system;
 
 public class customers {
+    private String customer_ID;
+    private String customer_Name;
+    private String customer_PhoneNo;
+    private String customer_Email;
+
+    public String getCustomer_ID() {
+        return customer_ID;
+    }
+
+    public void setCustomer_ID(String customer_ID) {
+        this.customer_ID = customer_ID;
+    }
+
+    public String getCustomer_Name() {
+        return customer_Name;
+    }
+
+    public void setCustomer_Name(String customer_Name) {
+        this.customer_Name = customer_Name;
+    }
+
+    public String getCustomer_PhoneNo() {
+        return customer_PhoneNo;
+    }
+
+    public void setCustomer_PhoneNo(String customer_PhoneNo) {
+        this.customer_PhoneNo = customer_PhoneNo;
+    }
+
+    public String getCustomer_Email() {
+        return customer_Email;
+    }
+
+    public void setCustomer_Email(String customer_Email) {
+        this.customer_Email = customer_Email;
+    }
 }
